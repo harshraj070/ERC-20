fff8b08859b9bbbc928edfb4fbbcebde805677d7
Harsh Raj <146359469+harshraj070@users.noreply.github.com>
2024-10-17 21:56:46 +0530

    Update ERC-20.sol

--- ERC-20.sol
+++ ERC-20.sol
@@ -5,9 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 
-contract MyToken is ERC20, ERC20Burnable, ERC20Permit, Ownable {
+contract MyToken is ERC20, ERC20Burnable, ERC20Permit, Ownable, Pausable, ERC20Snapshot {
     uint256 private _totalSupply = 1000000 * 10 ** decimals();
+    mapping(address => bool) private _blacklist;
+    address public taxRecipient;
+    uint256 public taxPercentage;
 
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
         _mint(msg.sender, _totalSupply);
@@ -21,21 +26,29 @@ contract MyToken is ERC20, ERC20Burnable, ERC20Permit, Ownable {
         return super.balanceOf(account);
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
+    function transfer(address recipient, uint256 amount) public override whenNotPaused returns (bool) {
+        require(!_blacklist[msg.sender], "Sender is blacklisted");
         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        return super.transfer(recipient, amount);
+
+        uint256 taxAmount = (amount * taxPercentage) / 100;
+        uint256 amountAfterTax = amount - taxAmount;
+
+        _transfer(msg.sender, taxRecipient, taxAmount);
+        return super.transfer(recipient, amountAfterTax);
     }
 
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public override returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) public override whenNotPaused returns (bool) {
+        require(!_blacklist[sender], "Sender is blacklisted");
         require(balanceOf(sender) >= amount, "Sender does not have enough tokens");
-        return super.transferFrom(sender, recipient, amount);
+
+        uint256 taxAmount = (amount * taxPercentage) / 100;
+        uint256 amountAfterTax = amount - taxAmount;
+
+        _transfer(sender, taxRecipient, taxAmount);
+        return super.transferFrom(sender, recipient, amountAfterTax);
     }
 
-    function approve(address spender, uint256 amount) public override returns (bool) {
+    function approve(address spender, uint256 amount) public override whenNotPaused returns (bool) {
         return super.approve(spender, amount);
     }
 
@@ -46,5 +59,47 @@ contract MyToken is ERC20, ERC20Burnable, ERC20Permit, Ownable {
     function burn(uint256 amount) public override {
         _burn(msg.sender, amount);
         _totalSupply -= amount;
+    }
+
+    function mint(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+        _totalSupply += amount;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function setTaxRecipient(address _taxRecipient) public onlyOwner {
+        taxRecipient = _taxRecipient;
+    }
+
+    function setTaxPercentage(uint256 _taxPercentage) public onlyOwner {
+        require(_taxPercentage <= 100, "Tax percentage cannot exceed 100");
+        taxPercentage = _taxPercentage;
+    }
+
+    function blacklist(address account) public onlyOwner {
+        _blacklist[account] = true;
+    }
+
+    function removeFromBlacklist(address account) public onlyOwner {
+        _blacklist[account] = false;
+    }
+
+    function isBlacklisted(address account) public view returns (bool) {
+        return _blacklist[account];
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Snapshot) {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
